7a4962b80495624ba275d235eb9a35506129e63b
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-01-17 16:51:35 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,14 @@
+public class Largest {
+    public static void main(String[] args) {
+        int[] arr = {34, 67, 23, 89, 2, 90, 45};
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("The largest number in the array is: " + largest);
+    }
+}
