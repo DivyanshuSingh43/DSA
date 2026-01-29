8b5c815b5cd41cabae2cfa97c43e4cfa4cdc927c
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-01-29 23:52:02 +0530

    Update Sum.java

--- ARRAYS/Sum.java
+++ ARRAYS/Sum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Sum {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter any number: ");
         int a = sc.nextInt();
