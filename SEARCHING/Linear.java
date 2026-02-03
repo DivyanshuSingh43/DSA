538eba23f617ced8f9e1c42006223ec542e84921
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-02-04 02:18:49 +0530

    Update Linear.java

--- SEARCHING/Linear.java
+++ SEARCHING/Linear.java
@@ -7,7 +7,7 @@ class Linear {
                 }
             }
                         return -1;
-                    }
+                   }
     public static void main(String[] args) {
 
         int num[]={10,20,30,43,50};
