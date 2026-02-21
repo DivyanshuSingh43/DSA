acfbf0f4c77ba464ed9fd7b5489566d166731d83
DivyanshuSingh43 <singhdivyanshu0408@gmail.com>
2026-02-21 11:44:45 +0530

    Update Largest.java

--- ARRAYS/Largest.java
+++ ARRAYS/Largest.java
@@ -4,7 +4,7 @@ public class Largest {
         int largest = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > largest) {
+            if (arr[i] > largest) { 
                 largest = arr[i];
             }  
         }  
